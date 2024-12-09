aef06da473dcf7d3728f10fc1fbe4b50c90c66a7
Unai Sainz de la Maza Gamboa <unaihtc70@gmail.com>
2024-12-09 11:49:50 +0100

    disable snacks dashboard

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -11,16 +11,6 @@ return {
     -- quickfile = { enabled = true },
     -- statuscolumn = { enabled = true },
     words = { enabled = true },
-    dashboard = {
-      enabled = true,
-      -- compact_file layout
-      -- sections = {
-      --   { section = 'header' },
-      --   { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
-      --   { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-      --   { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-      --   { section = 'startup' },
-      -- },
-    },
+    -- dashboard = { enabled = true },
   },
 }
