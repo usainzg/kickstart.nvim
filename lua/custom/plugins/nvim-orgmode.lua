524fcfbb3d69695a25be119bd7dbc8e25dd2fe76
Unai Sainz de la Maza Gamboa <unaihtc70@gmail.com>
2025-02-28 08:25:35 +0100

    orgmode and update

--- lua/custom/plugins/nvim-orgmode.lua
+++ lua/custom/plugins/nvim-orgmode.lua
@@ -10,8 +10,8 @@ return {
   event = 'VeryLazy',
   config = function()
     require('orgmode').setup {
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = '~/org-mode/**/*',
+      org_default_notes_file = '~/org-mode/refile.org',
     }
     require('org-bullets').setup()
     -- require('headlines').setup()
